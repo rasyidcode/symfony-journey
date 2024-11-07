6c1d62b6adcd32df747706ec0a666211208be17b
rasyidcode <rasyidcode7@gmail.com>
2024-11-07 09:52:40 +0700

    doing refactor number #5

--- blog-app/index.php
+++ blog-app/index.php
@@ -1,8 +1,14 @@
 <?php
 
 require_once 'model.php';
+require_once 'controller.php';
 
-$posts = get_all_posts();
-
-// include the HTML presentation code
-require 'templates/list.php';
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if ($uri === '/' || $uri === '/index.php') {
+    list_action();
+} else if ($uri === '/index.php/show' && isset($_GET['id'])) {
+    show_action($_GET['id']);
+} else {
+    header('HTTP/1.1 404 Not Found');
+    echo '<html lang="en"><body><h1>Page Not Found</h1></body></html>';
+}
