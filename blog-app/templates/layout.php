f1eced60a5a01093e176816a727de3119b93ec47
rasyidcode <rasyidcode7@gmail.com>
2024-11-07 09:33:32 +0700

    doing refactor number #3

---
+++ blog-app/templates/layout.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <?php echo $content; ?>
+</body>
+</html>
--- blog-app/templates/list.php
+++ blog-app/templates/list.php
@@ -1,18 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>List of Posts</title>
-</head>
-<body>
+<?php $title = 'List of Posts' ?>
+
+<?php ob_start() ?>
 <h1>List of Posts</h1>
 <ul>
-    <?php foreach ($posts as $post): ?>
-        <li>
-            <a href="/show.php?id=<?php echo $post['id']; ?>">
-                <?php echo $post['title']; ?>
-            </a>
-        </li>
+    <?php foreach ($posts as $post) : ?>
+    <li>
+        <a href="/show.php?id=<?php echo $post['id'] ?>">
+            <?php echo $post['title'] ?>
+        </a>
+    </li>
     <?php endforeach; ?>
 </ul>
-</body>
-</html>
+
+<?php $content = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
