6c2b76a56d169f9b7c52dca1dd02f6f2bdeb8914
rasyidcode <rasyidcode7@gmail.com>
2024-11-07 09:39:07 +0700

    doing refactor number #4

--- blog-app/model.php
+++ blog-app/model.php
@@ -25,3 +25,18 @@ function get_all_posts(): array
 
     return $posts;
 }
+
+function get_post_by_id(int $id): array
+{
+    $connection = open_database_connection();
+
+    $statement = $connection->prepare("SELECT * FROM posts WHERE id = :id");
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->execute(['id' => $id]);
+
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+    close_database_connection($connection);
+
+    return $row;
+}
