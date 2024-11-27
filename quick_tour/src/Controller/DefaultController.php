6af7ed37e8b455b08b33a23d1d78a4be0f6484df
rasyidcode <rasyidcode7@gmail.com>
2024-11-27 20:20:17 +0700

    next tour update

--- quick_tour/src/Controller/DefaultController.php
+++ quick_tour/src/Controller/DefaultController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
 
-    #[Route('/hello/{name}', name: 'index')]
+    #[Route('/hello/{name}', name: 'index', methods: ['GET'])]
     public function index(string $name): Response
     {
-        return new Response("Hello $name!");
+        return $this->render('default/index.html.twig', [
+            'name' => $name
+        ]);
     }
 
     #[Route('/simplicity', methods: ['GET'])]
